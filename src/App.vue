622fc4f6ef43ea5f3f83491ad1ed2ad1ea2115ce
ntivovirus <120092883+ntivovirus@users.noreply.github.com>
2023-08-04 09:45:16 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
 <template>
   <v-app class="grey lighten-4">
-    <Navbar/>
     <v-main class="mx-1 mb-1">
       <router-view/>
-    </v-main>
+    </v-main> 
   </v-app>
 </template>
 
-<script>
-import Navbar from '@/components/Navbar.vue'; //Importing the Navbar component
-
-export default {
-  name: 'App',
-
-  components: { Navbar}, // registering the Navbar component
-
-  data: () => ({
-    //
-  }),
-};
-</script>
