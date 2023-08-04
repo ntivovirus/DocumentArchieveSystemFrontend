f02247b30fb3b0c8d2a47936744759de2a19f0a3
ntivovirus <120092883+ntivovirus@users.noreply.github.com>
2023-08-04 09:36:56 +0200

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CorrespondenceView from '../views/CorrespondenceView.vue'
 import DocumentView from '../views/HomeView.vue'
-
+import Navbar from '@/components/Navbar.vue'
 
 
 Vue.use(VueRouter)
@@ -11,50 +11,73 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  }, 
-  {
-    path: '/file',
-    name: 'file',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/FileView.vue')
-  },
-  {
-    path: '/correspondence',
-    name: 'correspondence',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/CorrespondenceView.vue')
-    // component: CorrespondenceView
+    component: Navbar,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/HomeView.vue')
+       
+      },
+      {
+        path: '/file',
+        name: 'file',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/FileView.vue')
+       
+      },
+      {
+        path: '/correspondence',
+        name: 'correspondence',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/CorrespondenceView.vue')
+        // component: CorrespondenceView
+    
+      },
+      {
+        path: '/document',
+        name: 'document',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/DocumentView.vue')
+      },
+      {
+        path: '/user',
+        name: 'user',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/UserView.vue')
+      },
+      {
+        path: '/usersetting',
+        name: 'usersetting', 
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/UserSettingView.vue')
+      }
 
-  },
+    ]
+
+  }, 
+  
   {
-    path: '/document',
-    name: 'document',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/DocumentView.vue')
-  },
-  {
-    path: '/user',
-    name: 'user',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/UserView.vue')
-  },
-  {
-    path: '/usersetting',
-    name: 'usersetting',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/UserSettingView.vue')
+    // component : login
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
   }
 ]
 
