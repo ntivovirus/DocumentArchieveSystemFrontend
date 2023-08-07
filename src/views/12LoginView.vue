01cb5f4b3692cc661d329bbeba5f18b695d796d7
ntivovirus <120092883+ntivovirus@users.noreply.github.com>
2023-08-07 08:56:58 +0200

    Login view added

---
+++ src/views/12LoginView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <v-container fluid>
+      <v-row>
+        <v-col>
+          <v-card class="px-4">
+                            <v-card-text>
+                                <v-form ref="loginForm" v-model="valid" lazy-validation>
+                                    <v-row>
+                                        <v-col cols="12">
+                                            <v-text-field v-model="loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field v-model="loginPassword" :append-icon="show1?'eye':'eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
+                                        </v-col>
+                                        <v-col class="d-flex" cols="12" sm="6" xsm="12">
+                                        </v-col>
+                                        <v-spacer></v-spacer>
+                                        <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                                            <v-btn x-large block :disabled="!valid" color="success" @click="validate"> Login </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-form>
+                            </v-card-text>
+                        </v-card>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "login", // name of component view
+
+  data() {
+    return {
+
+      
+    };
+  },
+
+  methods: {
+
+    
+  },
+
+  mounted() {
+    
+  },
+};
+</script>
